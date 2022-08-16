7dcd9d8b24dcf4fa1a58545a17ac511864d3f05a
Mark Seemann <mark@ploeh.dk>
2022-08-16 17:42:40 +0200

    Add first test case

--- src/Range.hs
+++ src/Range.hs
@@ -1,1 +1,5 @@
 module Range where
+
+data Endpoint a = Open a | Closed a deriving (Eq, Show)
+
+contains _ _ = True
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,18 @@
+{-# OPTIONS_GHC -Wno-type-defaults #-}
 module Main where
 
+import Range
 import Test.HUnit
 import Test.Framework.Providers.HUnit (hUnitTestToTests)
 import Test.Framework (defaultMain)
 
 main :: IO ()
-main = defaultMain $ hUnitTestToTests $ TestList []
+main = defaultMain $ hUnitTestToTests $ TestList [
+  "integer range contains" ~: do
+    (r, candidate, expected) <-
+      [
+        ((Closed 2, Open 6), [2,4], True)
+      ]
+    let actual = r `contains` candidate
+    return $ expected ~=? actual
+  ]
