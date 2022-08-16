0d45f79623824ba14e5e75db56656a99829e1249
Mark Seemann <mark@ploeh.dk>
2022-08-16 21:15:22 +0200

    Eta reduction
    Also, more compact `case` expressions. This is terse enough, I think...

--- src/Range.hs
+++ src/Range.hs
@@ -5,13 +5,11 @@ import Control.Applicative (Applicative(liftA2))
 data Endpoint a = Open a | Closed a deriving (Eq, Show)
 
 contains :: (Foldable t, Ord a) => (Endpoint a, Endpoint a) -> t a -> Bool
-contains (lowerBound, upperBound) candidate =
-  let isHighEnough =
-        case lowerBound of
-          Closed x -> (x <=)
-          Open   x -> (x <)
-      isLowEnough =
-        case upperBound of
-          Closed y -> (<= y)
-          Open   y ->  (< y)
-  in all (liftA2 (&&) isHighEnough isLowEnough) candidate
+contains (lowerBound, upperBound) =
+  let isHighEnough = case lowerBound of
+        Closed x -> (x <=)
+        Open   x -> (x <)
+      isLowEnough = case upperBound of
+        Closed y -> (<= y)
+        Open   y ->  (< y)
+  in all (liftA2 (&&) isHighEnough isLowEnough)
